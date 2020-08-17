4b7ccf13330d0097fc1281d09b3975d8df4918d9
Xiaoyi Liu <lxy878@gmail.com>
2020-08-17 15:35:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    document.body.innerHTML = json.id;
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    document.body.innerHTML = error.message;
+  });
+
+};
